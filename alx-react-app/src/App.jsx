3a80394f66da1c1b1e2c2c9918171d498dd8571f
unknown <docziet@gmail.com>
2025-11-21 00:44:46 +0100

    Full react basics up to project 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +14,13 @@ function App() {
   return (
     <>
       <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <Footer />  
+      </div>
+      {/* <div className="logos">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +39,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }

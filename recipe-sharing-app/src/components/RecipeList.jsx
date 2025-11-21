0875075b3ee3a0ce90d366ed87c1003017102156
unknown <docziet@gmail.com>
2025-11-21 14:08:21 +0100

    resolve

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.filteredRecipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
   const allRecipes = useRecipeStore((state) => state.recipes);
   const searchTerm = useRecipeStore((state) => state.searchTerm);
 
+  // decide which list to show
   const listToDisplay =
-    searchTerm.trim().length > 0 ? recipes : allRecipes;
+    searchTerm.trim().length > 0 ? filteredRecipes : allRecipes;
 
   return (
     <div>
       {listToDisplay.map((recipe) => (
         <div key={recipe.id} style={{ marginBottom: "20px" }}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}

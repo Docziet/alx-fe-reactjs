d20bc0656898f34588f76815855fab6334072f0c
unknown <docziet@gmail.com>
2025-11-21 12:17:17 +0100

    Added the props and the my-company project.

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "20px",
+        backgroundColor: "#333",
+        color: "white",
+        display: "flex",
+        gap: "20px"
+      }}
+    >
+      <Link to="/" style={{ color: "white" }}>Home</Link>
+      <Link to="/about" style={{ color: "white" }}>About</Link>
+      <Link to="/services" style={{ color: "white" }}>Services</Link>
+      <Link to="/contact" style={{ color: "white" }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;

e8381dff63da34a3da215d6d40ce1504abbff0b2
unknown <docziet@gmail.com>
2025-11-21 11:44:54 +0100

    Working on the alx-react-app-new by copying it.

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <div>
+        <main
+          style={{
+          padding: "20px",
+          backgroundColor: "#f5f5f5",
+          minHeight: "200px"
+        }}
+        >
+          <p style={{textAlign:"center"}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    </div>
+    );
+}
+export default MainContent;
